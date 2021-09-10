40801b5427523113b32e2bdcc3a2adb5c569e5b8
Klas Mellbourn <klas@mellbourn.net>
2021-09-10 17:15:39 +0200

    move problem 3 to a method

--- app/src/main/java/com/capenta/problem3/App.java
+++ app/src/main/java/com/capenta/problem3/App.java
@@ -2,25 +2,29 @@ package com.capenta.problem3;
 
 public final class App {
 
-  private App() {
-  }
+	private App() {
+	}
 
-  private static int randomDigit() {
-    final var maxSingleDigit = 10;
-    return (int) (Math.random() * maxSingleDigit);
-  }
+	private static int randomDigit() {
+		final var maxSingleDigit = 10;
+		return (int) (Math.random() * maxSingleDigit);
+	}
 
-  /**
-   * Main program.
-   */
-  public static void main(String[] args) {
-    final int numberOfRows = 5;
-    final int numberOfDigits = 6;
-    for (var row = 0; row < numberOfRows; row++) {
-      for (var digit = 0; digit < numberOfDigits; digit++) {
-        System.out.print(randomDigit());
-      }
-      System.out.println();
-    }
-  }
+	private static void problem3() {
+		final int numberOfRows = 5;
+		final int numberOfDigits = 6;
+		for (var row = 0; row < numberOfRows; row++) {
+			for (var digit = 0; digit < numberOfDigits; digit++) {
+				System.out.print(randomDigit());
+			}
+			System.out.println();
+		}
+	}
+
+	/**
+	 * Main program.
+	 */
+	public static void main(String[] args) {
+		problem3();
+	}
 }
